b63b0d4ef4f117f8cb3908b927a26bfc5bc8203b
alokkumarak <68193957+alokkumarak@users.noreply.github.com>
2021-07-20 12:38:00 +0530

    Add files via upload

---
+++ build/precache-manifest.faf18a5349b6447f4b75b10f84c310e1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ba3739b661f90288753115c5c65bc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea4c9f84ea5b8583c23a",
+    "url": "/static/css/2.ca95fee5.chunk.css"
+  },
+  {
+    "revision": "139be04fd77f1a4dcb40",
+    "url": "/static/css/main.4e2de4dc.chunk.css"
+  },
+  {
+    "revision": "ea4c9f84ea5b8583c23a",
+    "url": "/static/js/2.5911df39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5911df39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "139be04fd77f1a4dcb40",
+    "url": "/static/js/main.5a3a1c76.chunk.js"
+  },
+  {
+    "revision": "66c5747557bea68eecec",
+    "url": "/static/js/runtime-main.62fb8e59.js"
+  },
+  {
+    "revision": "82918e0b6de66262f08c8b5905f0bf65",
+    "url": "/static/media/t.82918e0b.jpeg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
